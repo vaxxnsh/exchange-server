634789ae2132c0cec764bcc7269ab60e19058fbc
vaxxnsh <kakshit817@gmail.com>
2025-06-24 00:07:48 +0530

    testing phase start...

--- ws-server/src/index.ts
+++ ws-server/src/index.ts
@@ -1,7 +1,8 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket} from "ws";
+import { UserManager } from "./UserManager";
 
 const wss = new WebSocketServer({ port: 3001 });
 
 wss.on("connection", (ws : WebSocket) => {
-    // UserManager.getInstance().addUser(ws);
+    UserManager.getInstance().addUser(ws);
 });
